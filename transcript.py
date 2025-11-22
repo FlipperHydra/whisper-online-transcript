c56285cddf6d3887167490826c6c824ca0eb1a7a
FlipperHydra <s.clayto75@gmail.com>
2025-11-22 13:34:59 -0500

    Add files via upload

--- transcript.py
+++ transcript.py
@@ -1,16 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI, HTTPException, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
 import whisper
 import os
-
-import shutil
-
 model = whisper.load_model("turbo")
 
+#Transcribes
 def stt(filename: str):
     if not os.path.exists(filename):
         raise FileNotFoundError(f"Audio file not found: {filename}")
@@ -18,10 +16,10 @@ def stt(filename: str):
     result = model.transcribe(filename)
     return result["text"]
 
-
+#Create instance of the FastAPI class
 app = FastAPI()
 
-#CONNECTION TO FRONT 
+#Allows browser to sync backend and frontend
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -29,26 +27,29 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+#CONNECT TO FRONTEND
 @app.get("/")
 def read_root():
     return FileResponse("index.html")
 
 @app.post("/upload")
 async def upload_audio(file: UploadFile = File(...)):
-    file_path = f"temp_{file.filename}"
+    #Splits into 1Mb chunks
+    file_path = f"temp_{file.filename}" 
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        while chunk := await file.read(1024 * 1024):  
+            buffer.write(chunk)
     
     transcription = stt(file_path)
     os.remove(file_path)
     
     return {"text": transcription, "filename": file.filename}
-#CONNECTION TO FRONT
+#CONNECT TO FRONTEND
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        #reload=True
-        )
+        timeout_keep_alive=4800  
+    )
 
+#If suggestions for efficiency, email: doe48944067@gmail.com
